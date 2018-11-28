6344c343233c86448964fe09ed52455129e5c0ac
RizalCck <45404308+RizalCck@users.noreply.github.com>
2018-11-28 09:34:06 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 ///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
 ///Ganti xxxxxxx dengan punya kalian
 ///Capture dulu apk nya ambil yang di butuhkan
-$ticket = ""sHp2r312otmCpnGns3mCqJhovNiBiozMu7qJsbPbe8-xsIaxfazNzoO2mJu0hYFmhI2wzIt3e5mvuo2wtbWdy62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ":
+$ticket = ""sHp2r312otmCpnGns3mCqJhovNiBiozMu7qJsbPbe8-xsIaxfazNzoO2mJu0hYFmhI2wzIt3e5mvuo2wtbWdy62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
 $meid = "4e3387acb0bee8c9";
 $os="android";
 $sign="81fca9044f6c0307fc0ddb4f1c334d821a065ea08c0f3f9ce3e6c94166d0fa18";
